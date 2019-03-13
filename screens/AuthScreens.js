c77eb9c6fbe9062c0448a3e07f232bbe5c9617d1
Nazar Bayramsahatov <n.bayramsahatov@akyllytilsimat.local>
2019-03-13 16:52:06 +0500

    upd

---
+++ screens/AuthScreens.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import {View,Text} from 'react-native';
+
+class AuthScreens extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Auth Screen</Text>
+                <Text>Auth Screen</Text>
+                <Text>Auth Screen</Text>
+                <Text>Auth Screen</Text>
+                <Text>Auth Screen</Text>
+                <Text>Auth Screen</Text>
+            </View>
+        )
+    }
+}
+
+export default AuthScreens;
